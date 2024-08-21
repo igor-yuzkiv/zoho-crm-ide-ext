cc17dd9ba569da128d6d6d47031b544735fa4abc
Igor Yuzkiv <igor97w@gmail.com>
2024-08-21 10:25:20 +0300

    functions

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ export const CRM_DOMAINS = [
     'crm.zoho.com.au',
     'crm.zoho.eu',
     'crm.zoho.in',
+    'crm.zoho.com',
     'crm.zoho.com.cn',
     'crm.zoho.jp',
     'crmsandbox.zoho.com.au',
