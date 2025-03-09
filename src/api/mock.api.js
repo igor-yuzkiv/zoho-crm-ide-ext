680e9c675c4888f9d5272175e956a3220eabdac7
Igor Yuzkiv <igor97w@gmail.com>
2025-03-09 13:45:52 +0200

    service provider

--- src/api/mock.api.js
+++ src/api/mock.api.js
@@ -1,5 +1,5 @@
 export async function fetchMockChromeTabsQuery() {
-    const data = await import('@/data/chrome_tabs.json')
+    const data = await import('@/mock_data/chrome_tabs.json')
 
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -9,7 +9,7 @@ export async function fetchMockChromeTabsQuery() {
 }
 
 export async function fetchMockCrmFunctions(start = 0, limit = 100) {
-    const data = await import('@/data/crm_functions_list.json')
+    const data = await import('@/mock_data/crm_functions_list.json')
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(data.default.slice(start, start + limit))
@@ -18,7 +18,7 @@ export async function fetchMockCrmFunctions(start = 0, limit = 100) {
 }
 
 export async function fetchMockCrmFunctionDetails(id) {
-    const data = await import('@/data/crm_functions_details.json')
+    const data = await import('@/mock_data/crm_functions_details.json')
 
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -28,7 +28,7 @@ export async function fetchMockCrmFunctionDetails(id) {
 }
 
 export async function fetchMockBooksFunctions() {
-    const data = await import('@/data/books_functions_details.json')
+    const data = await import('@/mock_data/books_functions_details.json')
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,6 @@
-<script setup>
-import { useWorkspaceStore } from '@/store/useWorkspaceStore.js'
-import Button from 'primevue/button'
-import { fetchZohoFinanceFunctionDetails, fetchZohoFinanceFunctions } from '@/api/zoho-finance.api.js'
-
-const workspace = useWorkspaceStore()
-
-async function test() {
-    //await fetchZohoFinanceFunctions(workspace.provider.tab.id, workspace.provider.metadata.org_id)
-
-    const functionsDetails = [];
-
-    const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-    for (const item of workspace.functions) {
-        const response = await fetchZohoFinanceFunctionDetails(
-            workspace.provider.tab.id,
-            workspace.provider.metadata.org_id,
-            item.id
-        )
-
-        if (response) {
-            functionsDetails.push(response);
-        }
-
-        await sleep(200);
-    }
-
-    console.log(functionsDetails);
-}
-</script>
+<script setup></script>
 <template>
     <div class="flex h-full w-full flex-col items-center justify-center">
-        <div class="text-4xl text-gray-500" @click="test">
-            Function not selected
-        </div>
+        <div class="text-4xl text-gray-500">Function not selected</div>
     </div>
 </template>
